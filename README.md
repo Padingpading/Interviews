3324eabe48b926ae9b7b4ae207399a3f8d162708
pad <449196538@qq.com>
2022-04-29 20:26:06 +0800

    111

--- README.md
+++
@@ -1,3 +0,0 @@
-# Interviews
-
-# 启动配置
---
+++ base/src/main/java/com/padingpading/interview/collection/b_hashmap/Test.java
@@ -0,0 +1,17 @@
+package com.padingpading.interview.collection.b_hashmap;
+
+import java.util.HashMap;
+
+/**
+ * @author libin
+ * @description
+ * @date 2022-04-27
+ */
+public class Test {
+    
+    public static void main(String[] args) {
+    
+        HashMap hashMap = new HashMap();
+    }
+    
+}
