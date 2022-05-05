13b699f48adee92551152c39ad56898c8a24a337
pad <449196538@qq.com>
2022-05-05 17:20:03 +0800

    111

---
+++ base/src/main/java/com/padingpading/interview/collection/b_vector/VectorTest.java
@@ -0,0 +1,17 @@
+package com.padingpading.interview.collection.b_vector;
+
+import java.util.Vector;
+
+/**
+ * @author libin
+ * @description
+ * @date 2022-05-05
+ */
+public class VectorTest {
+    
+    public static void main(String[] args) {
+        
+        Vector vector = new Vector();
+    }
+    
+}
