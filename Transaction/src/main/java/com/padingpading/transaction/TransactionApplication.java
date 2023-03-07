19db5e251c07aa7825769ea0b7a8baba596b59ed
libin <449196538@qq.com>
2023-03-07 22:48:39 +0800

    提交

--- Transaction/src/main/java/com/padingpading/transaction/TransactionApplication.java
+++ Transaction/src/main/java/com/padingpading/transaction/TransactionApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransactionApplication {
     
-    public static void main(String[] args) {
-        SpringApplication.run(TransactionApplication.class, args);
-    }
+  //  public static void main(String[] args) {
+//        SpringApplication.run(TransactionApplication.class, args);
+//    }
     
+        public static void main(String[] args) {
+            testStrictfp();
+        }
+        
+        public strictfp static void testStrictfp() {
+            float aFloat = 0.6666666666666666666f;
+            double aDouble = 0.88888888888888888d;
+            double sum = aFloat + aDouble;
+            System.out.println("sum: " + sum);
+        }
 }
--- base/src/main/java/com/padingpading/java/jdk15/B_Hidden.java
+++ base/src/main/java/com/padingpading/java/jdk15/B_Hidden.java
@@ -1,30 +1,30 @@
 package com.padingpading.java.jdk15;
+
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.Base64;
 
 /**
- * 让开发者可以引入一个无法被其他地方发现使用，
- * 且类的生命周期有限的类。这对运行时动态生成类的使用方式十分有利，可以减少内存占用
+ * 让开发者可以引入一个无法被其他地方发现使用， 且类的生命周期有限的类。这对运行时动态生成类的使用方式十分有利，可以减少内存占用
  */
 public class B_Hidden {
     
-        private static String CLASS_INFO = "yv66vgAAADQAFAoAAgADBwAEDAAFAAYBABBqYXZhL2xhbmcvT2JqZWN0AQAGPGluaXQ+AQADKClWCAAIAQAOd3d3LndkYnl0ZS5jb20HAAoBABVjb20vd2RieXRlL0pFUDM3MVRlc3QBAARDb2RlAQAPTGluZU51bWJlclRhYmxlAQASTG9jYWxWYXJpYWJsZVRhYmxlAQAEdGhpcwEAF0xjb20vd2RieXRlL0pFUDM3MVRlc3Q7AQAGbG9va3VwAQAUKClMamF2YS9sYW5nL1N0cmluZzsBAApTb3VyY2VGaWxlAQAPSkVQMzcxVGVzdC5qYXZhACEACQACAAAAAAACAAEABQAGAAEACwAAAC8AAQABAAAABSq3AAGxAAAAAgAMAAAABgABAAAAAwANAAAADAABAAAABQAOAA8AAAAJABAAEQABAAsAAAAbAAEAAAAAAAMSB7AAAAABAAwAAAAGAAEAAAAEAAEAEgAAAAIAEw==";
+    private static String CLASS_INFO = "yv66vgAAADQAFAoAAgADBwAEDAAFAAYBABBqYXZhL2xhbmcvT2JqZWN0AQAGPGluaXQ+AQADKClWCAAIAQAOd3d3LndkYnl0ZS5jb20HAAoBABVjb20vd2RieXRlL0pFUDM3MVRlc3QBAARDb2RlAQAPTGluZU51bWJlclRhYmxlAQASTG9jYWxWYXJpYWJsZVRhYmxlAQAEdGhpcwEAF0xjb20vd2RieXRlL0pFUDM3MVRlc3Q7AQAGbG9va3VwAQAUKClMamF2YS9sYW5nL1N0cmluZzsBAApTb3VyY2VGaWxlAQAPSkVQMzcxVGVzdC5qYXZhACEACQACAAAAAAACAAEABQAGAAEACwAAAC8AAQABAAAABSq3AAGxAAAAAgAMAAAABgABAAAAAwANAAAADAABAAAABQAOAA8AAAAJABAAEQABAAsAAAAbAAEAAAAAAAMSB7AAAAABAAwAAAAGAAEAAAAEAAEAEgAAAAIAEw==";
+    
+    public static void main(String[] args) throws Throwable {
+        byte[] classInBytes = Base64.getDecoder().decode(CLASS_INFO);
+        Class<?> proxy = MethodHandles.lookup()
+                .defineHiddenClass(classInBytes, true, MethodHandles.Lookup.ClassOption.NESTMATE).lookupClass();
         
-        public static void main(String[] args) throws Throwable {
-            byte[] classInBytes = Base64.getDecoder().decode(CLASS_INFO);
-            Class<?> proxy = MethodHandles.lookup()
-                    .defineHiddenClass(classInBytes, true, MethodHandles.Lookup.ClassOption.NESTMATE)
-                    .lookupClass();
-            
-            System.out.println(proxy.getName());
-            MethodHandle mh = MethodHandles.lookup().findStatic(proxy, "lookup", MethodType.methodType(String.class));
-            String result = (String) mh.invokeExact();
-            System.out.println(result);
-        }
+        System.out.println(proxy.getName());
+        MethodHandle mh = MethodHandles.lookup().findStatic(proxy, "lookup", MethodType.methodType(String.class));
+        String result = (String) mh.invokeExact();
+        System.out.println(result);
+    }
     
     public static class JEP371Test {
+        
         public static String lookup() {
             return "www.wdbyte.com";
         }
